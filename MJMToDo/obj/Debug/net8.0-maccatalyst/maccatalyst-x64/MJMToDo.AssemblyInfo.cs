ff925da00f1c31c771e7e4b2fcfbb068272e216f
Mohamad javad Naeeni <naeenijavad@yahoo.com>
2024-10-16 20:17:42 +0330

    ignore debug directory

--- MJMToDo/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/MJMToDo.AssemblyInfo.cs
+++ MJMToDo/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/MJMToDo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MJMToDo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0b0c9714ab2d4becb0ca30a7b39d25451cdeb828")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+112ae5ac6fa6e568e5ce85e7094bd13327e017f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MJMToDo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MJMToDo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
