9beee786e3486c45defa5feef0aa63f2ea42567c
Mahdi Naeeni <Mahdi Naeeni@DESKTOP-SMKQFMR>
2024-10-30 22:47:47 +0330

    delete appshell.xaml

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
-
+using MJMToDo.Repositories;
 namespace MJMToDo
 {
     public static class MauiProgram
@@ -13,13 +13,33 @@ namespace MJMToDo
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
-
-#if DEBUG
-    		builder.Logging.AddDebug();
-#endif
-
+                })
+                .RegisterServices()
+                .RegisterViewModels()
+                .RegisterViews();
             return builder.Build();
+        }
+        public static MauiAppBuilder RegisterServices(this
+    MauiAppBuilder mauiAppBuilder)
+        {
+            mauiAppBuilder.Services.AddSingleton<ITodoItemRepository, TodoItemRepository>();
+            return mauiAppBuilder;
+        }
+        public static MauiAppBuilder RegisterViewModels(this
+    MauiAppBuilder mauiAppBuilder)
+        {
+            mauiAppBuilder.Services.AddTransient<ViewModels.
+   MainViewModel>();
+            mauiAppBuilder.Services.AddTransient<ViewModels.
+    ItemViewModel>();
+            return mauiAppBuilder;
+        }
+        public static MauiAppBuilder RegisterViews(this
+    MauiAppBuilder mauiAppBuilder)
+        {
+            mauiAppBuilder.Services.AddTransient<Views.MainView>();
+            mauiAppBuilder.Services.AddTransient<Views.ItemView>();
+            return mauiAppBuilder;
         }
     }
 }
