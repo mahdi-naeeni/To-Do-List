ab742b7857a63b5762e408b469ea02431b245f36
Mahdi Naeeni <Mahdi Naeeni@DESKTOP-SMKQFMR>
2024-11-04 22:35:55 +0330

    Navigating from MainView to ItemView to add a new item Adding new items to the list Binding ListView in MainView

--- ViewModels/ItemViewModel.cs
+++ ViewModels/ItemViewModel.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MJMToDo.Models;
 using MJMToDo.Repositories;
 
 namespace MJMToDo.ViewModels
 {
-    public class ItemViewModel : ViewModel
+    public partial class ItemViewModel : ViewModel
     {
         private readonly ITodoItemRepository repository;
+
+        public TodoItem Item { get; }
+
+        [ObservableProperty]
+        TodoItem item;
         public ItemViewModel(ITodoItemRepository repository)
         {
             this.repository = repository;
+            Item = new TodoItem() { Due = DateTime.Now.AddDays(1) };
+        }
+        [RelayCommand]
+        public async Task SaveAsync()
+        {
+            await repository.AddOrUpdateAsync(Item);
+            await Navigation.PopAsync();
         }
     }
 }
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -6,16 +6,28 @@ using System.Text;
 using System.Threading.Tasks;
 using MJMToDo.Views;
 using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using MJMToDo.Models;
 
 namespace MJMToDo.ViewModels
 {
     public partial class MainViewModel : ViewModel
     {
+
         private readonly ITodoItemRepository repository;
         private readonly IServiceProvider services;
+        [ObservableProperty]
+        ObservableCollection<TodoItemViewModel> items;
+
+        public ObservableCollection<TodoItemViewModel> Items { get; private set; }
 
         public MainViewModel(ITodoItemRepository repository, IServiceProvider services)
         {
+            repository.OnItemAdded += (sender, item) =>
+            items.Add(CreateTodoItemViewModel(item));
+            repository.OnItemUpdated += (sender, item) =>
+            Task.Run(async () => await LoadDataAsync());
             this.repository = repository;
             this.services = services;
             Task.Run(async () => await LoadDataAsync());
@@ -23,8 +35,21 @@ namespace MJMToDo.ViewModels
         }
         private async Task LoadDataAsync()
         {
+            var items = await repository.GetItemsAsync();
+            var itemViewModels = items.Select(i => CreateTodoItemViewModel(i));
+            Items = new ObservableCollection<TodoItemViewModel>
+        (itemViewModels);
         }
         [RelayCommand]
         public async Task AddItemAsync() => await Navigation.PushAsync(services.GetRequiredService<ItemView>());
+        private TodoItemViewModel CreateTodoItemViewModel(TodoItem item)
+        {
+            var itemViewModel = new TodoItemViewModel(item);
+            itemViewModel.ItemStatusChanged += ItemStatusChanged;
+            return itemViewModel;
+        }
+        private void ItemStatusChanged(object sender, EventArgs e)
+        {
+        }
     }
 }
