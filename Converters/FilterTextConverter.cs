07786dfbde905daa68d602f5baf8f56ee205ecaf
Mahdi Naeeni <Mahdi Naeeni@DESKTOP-SMKQFMR>
2024-11-24 20:50:58 +0330

    fix bugs

--- Converters/FilterTextConverter.cs
+++ Converters/FilterTextConverter.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System;
 using System.Globalization;
+namespace MJMToDo.Converters;
 internal class FilterTextConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
